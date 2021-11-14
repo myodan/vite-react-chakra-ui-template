00a7da3f5a7ea4e7b6f8ade8e6b7212f61243105
Jongho Hong <kr.myodan@gmail.com>
2021-11-14 22:57:45 +0900

    Initial commit

---
+++ src/components/app.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <ChakraProvider>
+      <div>Hello, React</div>
+    </ChakraProvider>
+  );
+};
+
+export default App;
